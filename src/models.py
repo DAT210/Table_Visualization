51934f037a58f5457e0ba0550ccf5eeea35507d1
Eiriksak <eiriksak@hotmail.com>
2018-10-25 14:13:24 +0200

    roomplan added to database

--- src/models.py
+++ src/models.py
@@ -3,8 +3,27 @@ from __init__ import db
 class Roomplan(db.Model):
     __tablename__ = 'roomplan'
 
-    id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, primary_key=True)
+    width = db.Column(db.Integer, default=700)
+    height = db.Column(db.Integer, default=500)
+    tables = db.relationship('Tables', backref='table', lazy=True)
+    walls = db.relationship('Walls', backref='wall', lazy=True)
+
+
+    def __init__(self, name):
+        self.name = name
+
+
+    def __repr__(self):
+        return '<title {}'.format(self.name)
+
+
+
+class Tables(db.Model):
+    __tablename__ = 'table'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, db.ForeignKey('roomplan.name'), nullable=False)
     xpos = db.Column(db.Integer)
     ypos = db.Column(db.Integer)
     width = db.Column(db.Integer)
@@ -31,7 +50,7 @@ class Walls(db.Model):
     __tablename__ = 'wall'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String, primary_key=True)
+    name = db.Column(db.String, db.ForeignKey('roomplan.name'), nullable=False)
     x_start = db.Column(db.Integer)
     x_end = db.Column(db.Integer)
     y_start = db.Column(db.Integer)
--- src/static/app.js
+++ src/static/app.js
@@ -1,1 +1,63 @@
 "use strict";
+var RoomVisualizer = /** @class */ (function () {
+    function RoomVisualizer(visualizer) {
+        this.tables = {};
+        this.visualizer = visualizer;
+        document.body.appendChild(this.visualizer.Wrapper);
+    }
+    Object.defineProperty(RoomVisualizer.prototype, "RoomPlan", {
+        set: function (roomPlan) {
+            this.roomPlan = roomPlan;
+            this.visualizer.Width = this.roomPlan.width;
+            this.visualizer.Height = this.roomPlan.height;
+            this.drawRoom();
+        },
+        enumerable: true,
+        configurable: true
+    });
+    RoomVisualizer.prototype.drawRoom = function () {
+        this.visualizer.Reset();
+        this.drawWallsAsPoly();
+        this.drawTables();
+    };
+    RoomVisualizer.prototype.drawWalls = function () {
+        if (!this.roomPlan)
+            return;
+        for (var _i = 0, _a = this.roomPlan.walls; _i < _a.length; _i++) {
+            var wall = _a[_i];
+            this.visualizer.AddLine(wall.from, wall.to);
+        }
+    };
+    RoomVisualizer.prototype.drawWallsAsPoly = function () {
+        if (!this.roomPlan)
+            return;
+        var wallPoints = this.roomPlan.walls.map(function (w) { return w.from; });
+        this.visualizer.AddPoly(wallPoints);
+    };
+    RoomVisualizer.prototype.drawTables = function () {
+        var _this = this;
+        if (!this.roomPlan)
+            return;
+        var _loop_1 = function (table) {
+            var rect = this_1.visualizer.AddRect(table.width, table.height, table.position, true, "table");
+            rect.OnClick = function () { console.log("Table " + table.id + " clicked!"); };
+            rect.OnMove = function () { _this.updateTablePos(table.id, rect.Position); };
+            this_1.tables[table.id] = rect;
+        };
+        var this_1 = this;
+        for (var _i = 0, _a = this.roomPlan.tables; _i < _a.length; _i++) {
+            var table = _a[_i];
+            _loop_1(table);
+        }
+    };
+    RoomVisualizer.prototype.updateTablePos = function (tableID, pos) {
+        if (this.roomPlan) {
+            for (var _i = 0, _a = this.roomPlan.tables; _i < _a.length; _i++) {
+                var t = _a[_i];
+                if (t.id == tableID)
+                    t.position = pos;
+            }
+        }
+    };
+    return RoomVisualizer;
+}());

b466efffce82d3d627eea796a98aebbe8e3b1a83
stitro <stiantrondsen@hotmail.com>
2018-10-25 12:04:20 +0200

    split  style

--- src/static/app.ts
+++ src/static/app.ts
@@ -22,3 +22,59 @@ interface IRoom {
     walls: IWall[]
 }
 
+class RoomVisualizer {
+    private visualizer: IInteractiveVisualizer;
+    private roomPlan: IRoom | undefined;
+    private tables: { [id: number]: IInteractiveVisualizerElement } = {};
+
+    constructor(visualizer: IInteractiveVisualizer) {
+        this.visualizer = visualizer;
+        document.body.appendChild(this.visualizer.Wrapper);
+    }
+
+    set RoomPlan(roomPlan: IRoom) {
+        this.roomPlan = roomPlan;
+        this.visualizer.Width = this.roomPlan.width;
+        this.visualizer.Height = this.roomPlan.height;
+        this.drawRoom();
+    }
+
+    private drawRoom(): void {
+        this.visualizer.Reset();
+        this.drawWallsAsPoly();
+        this.drawTables();
+    }
+    private drawWalls(): void {
+        if (!this.roomPlan) return;
+        for(let wall of this.roomPlan.walls) {
+            this.visualizer.AddLine(wall.from, wall.to);
+        }
+    }
+    private drawWallsAsPoly(): void {
+        if (!this.roomPlan) return;
+        const wallPoints = this.roomPlan.walls.map(w => w.from);
+        this.visualizer.AddPoly(wallPoints);
+    }
+    private drawTables(): void {
+        if (!this.roomPlan) return;
+        for(let table of this.roomPlan.tables) {
+            const rect = this.visualizer.AddRect(
+                table.width,
+                table.height,
+                table.position,
+                true,
+                "table"
+            );
+            rect.OnClick = () => { console.log("Table " + table.id + " clicked!") };
+            rect.OnMove = () => { this.updateTablePos(table.id, rect.Position) };
+            this.tables[table.id] = rect;
+        }
+    }
+    private updateTablePos(tableID: number, pos: IPoint): void {
+        if (this.roomPlan) {
+            for (let t of this.roomPlan.tables) {
+                if (t.id == tableID) t.position = pos;
+            }
+        }
+    }
+}

f7b625996cbf320369872ca1c4295454df166f65
stian <stiantrondsen@hotmail.com>
2018-11-11 10:45:08 +0100

    test scaffolding

--- src/static/app.test.ts
+++ src/static/app.test.ts
@@ -1,8 +1,27 @@
 import { expect } from 'chai';
+import './app.js';
+import './user';
+import './admin.js';
+import './visualizer';
 
-describe("TestTest", () => {
-    it("should return test", () => {
-        const test = "test";
-        expect(test).to.equal("test");
-    })
-})
+describe("API: get bookings", () => {
+    it("should return response with bookings", () => {
+        
+    });
+});
+describe("API: post tables", () => {
+    it("should return response with success", () => {
+        
+    });
+});
+
+describe("Admin to server: update roomplan", () => {
+    it("should return response with success", () => {
+        
+    });
+});
+describe("Admin to server: add roomplan", () => {
+    it("should return response with success", () => {
+        
+    });
+});
